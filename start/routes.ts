93dafc7b2965019092123151b58cadb5a82cfb06
rchrdkvcs <b34r.rk@gmail.com>
2025-02-22 17:15:29 +0100

    chore(auth): Auth disabled

--- start/routes.ts
+++ start/routes.ts
@@ -8,35 +8,27 @@
 */
 
 import router from '@adonisjs/core/services/router'
-import { middleware } from '#start/kernel'
-const RegisterController = () => import('#auth/controller/register_controller')
 
 const LoginController = () => import('#auth/controller/login_controller')
-// const RegisterController = () => import('#auth/controller/register_controller')
-
-const TeamsController = () => import('../app/teams/controller/teams_controller.js')
+const RegisterController = () => import('#auth/controller/register_controller')
+const TeamsController = () => import('#teams/controller/teams_controller')
 const PlayersController = () => import('#players/controller/players_controller')
-const SheetsController = () => import('#google/controller/sheets_controller')
-const BanphaseMessageController = () =>
-  import('../app/banphase/controller/banphase_message_controller.js')
 const GenerateUrlsController = () =>
   import('../app/banphase/controller/generate_urls_controller.js')
-import('../app/banphase/controller/generate_urls_controller.js')
+const BanphaseMessageController = () =>
+  import('../app/banphase/controller/banphase_message_controller.js')
+const SheetsController = () => import('#google/controller/sheets_controller')
 
 router.post('/login', [LoginController, 'execute'])
 router.post('/register', [RegisterController, 'execute'])
 
-router
-  .group(() => {
-    router.get('/teams', [TeamsController, 'render'])
-    router.get('/team/:id', [TeamsController, 'show'])
+router.get('/teams', [TeamsController, 'render'])
+router.get('/team/:id', [TeamsController, 'show'])
 
-    router.get('/players', [PlayersController, 'render'])
-    router.get('/player/:id', [PlayersController, 'show'])
+router.get('/players', [PlayersController, 'render'])
+router.get('/player/:id', [PlayersController, 'show'])
 
-    router.post('/banphase/generate/urls', [GenerateUrlsController, 'execute'])
-    router.post('/banphase/message', [BanphaseMessageController, 'execute'])
+router.post('/banphase/generate/urls', [GenerateUrlsController, 'execute'])
+router.post('/banphase/message', [BanphaseMessageController, 'execute'])
 
-    router.post('/sheet/sync', [SheetsController, 'execute'])
-  })
-  .use(middleware.auth())
+router.post('/sheet/sync', [SheetsController, 'execute'])
